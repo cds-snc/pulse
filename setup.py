fbf375f48b7fdd1d3a5ba12bf88a7c47ea238c25
David Buckley <buckley.w.david@gmail.com>
2018-04-18 11:20:27 -0400

    add package setup file

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name='pulse',
+    version='0.0.1',
+    long_description='',
+    author='CDS-SNC',
+    url='https://github.com/cds-snc/pulse',
+    packages=[
+        'data',
+    ],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+    ],
+)
