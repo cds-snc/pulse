66a94b97f6805538b993de63c5154223dd5d1712
David Buckley <buckley.w.david@gmail.com>
2018-05-04 17:44:03 -0400

    linting and type checking

--- pulse/models.py
+++ pulse/models.py
@@ -1,10 +1,7 @@
-import pymongo
-import os
 import io
 import datetime
 import csv
 import typing
-from werkzeug.local import LocalProxy
 from flask_pymongo import PyMongo
 from pulse.data import CSV_FIELDS, FIELD_MAPPING, LABELS
 
@@ -30,14 +27,17 @@ class Report:
     # analytics.participates (number)
 
     # Initialize a report with a given date.
-    def create(data):
+    @classmethod
+    def create(cls, data: typing.Dict) -> None:
         db.db.reports.insert_one(data.copy())
 
-    def report_time(report_date):
+    @classmethod
+    def report_time(cls, report_date: str) -> datetime.datetime:
         return datetime.datetime.strptime(report_date, "%Y-%m-%d")
 
+    @classmethod
     # There's only ever one.
-    def latest():
+    def latest(cls) -> typing.Dict:
         return db.db.reports.find_one({}, {'_id': False})
 
 
@@ -65,62 +65,72 @@ class Domain:
     # analytics: { ... }
     #
 
-    def create(data):
+    @classmethod
+    def create(cls, data: typing.Dict) -> None:
         return db.db.domains.insert_one(data.copy())
 
-    def create_all(iterable: typing.List[typing.Dict]):
+    @classmethod
+    def create_all(cls, iterable: typing.List[typing.Dict]) -> None:
         return db.db.domains.insert_many(iterable)
 
-    def update(domain_name, data):
+    @classmethod
+    def update(cls, domain_name: str, data: typing.Dict) -> None:
         return db.db.domains.update_one(
             {'domain': domain_name},
             {'$set': data},
         )
 
-    def add_report(domain_name, report_name, report):
+    @classmethod
+    def add_report(cls, domain_name: str, report_name: str, report: typing.Dict) -> None:
         return db.db.domains.update_one(
             {'domain': domain_name},
             {'$set': {report_name: report}}
         )
 
-    def find(domain_name):
+    @classmethod
+    def find(cls, domain_name: str) -> typing.Dict:
         return db.db.domains.find_one({'domain': domain_name}, {'_id': False})
 
     # Useful when you want to pull in all domain entries as peers,
     # such as reports which only look at parent domains, or
     # a flat CSV of all hostnames that match a report.
-    def eligible(report_name):
+    @classmethod
+    def eligible(cls, report_name: str) -> typing.Iterable[typing.Dict]:
         return db.db.domains.find(
             {f'{report_name}.eligible': True}, {'_id': False}
         )
 
     # Useful when you have mixed parent/subdomain reporting,
     # used for HTTPS but not yet others.
-    def eligible_parents(report_name):
+    @classmethod
+    def eligible_parents(cls, report_name: str) -> typing.Iterable[typing.Dict]:
         return db.db.domains.find(
             {f'{report_name}.eligible_zone': True, 'is_parent': True}, {'_id': False}
         )
 
     # Useful when you want to pull down subdomains of a particular
     # parent domain. Used for HTTPS expanded reports.
-    def eligible_for_domain(domain, report_name):
+    @classmethod
+    def eligible_for_domain(cls, domain: str, report_name: str) -> typing.Iterable[typing.Dict]:
         return db.db.domains.find(
             {f'{report_name}.eligible': True, 'base_domain': domain}, {'_id': False}
         )
 
-    def all():
+    @classmethod
+    def all(cls) -> typing.Iterable[typing.Dict]:
         return db.db.domains.find({}, {'_id': False})
 
-    def to_csv(domains, report_type):
+    @classmethod
+    def to_csv(cls, domains: typing.Iterable[typing.Dict], report_type: str) -> str:
         output = io.StringIO()
         writer = csv.writer(output, quoting=csv.QUOTE_NONNUMERIC)
 
-        def value_for(value):
+        def value_for(value: typing.Union[str, list, bool]) -> str:
             # if it's a list, convert it to a list of strings and join
-            if type(value) is list:
+            if isinstance(value, list):
                 value = [str(x) for x in value]
                 value = ", ".join(value)
-            elif type(value) is bool:
+            elif isinstance(value, bool):
                 value = {True: 'Yes', False: 'No'}[value]
             return value
 
@@ -184,25 +194,31 @@ class Agency:
     #
 
     # An agency which had at least 1 eligible domain.
-    def eligible(report_name):
+    @classmethod
+    def eligible(cls, report_name: str) -> typing.Iterable[typing.Dict]:
         return db.db.agencies.find({f'{report_name}.eligible': {'$gt': 0}}, {'_id': False})
 
     # Create a new Agency record with a given name, slug, and total domain count.
-    def create(data):
+    @classmethod
+    def create(cls, data: typing.Dict) -> None:
         return db.db.agencies.insert_one(data.copy()) # Copy dictionary to prevent mutation side effect
 
-    def create_all(iterable):
+    @classmethod
+    def create_all(cls, iterable: typing.Iterable[typing.Dict]) -> None:
         return db.db.agencies.insert_many(iterable)
 
     # For a given agency, add a report.
-    def add_report(slug, report_name, report):
+    @classmethod
+    def add_report(cls, slug: str, report_name: str, report: typing.Dict) -> None:
         return db.db.agencies.update_one(
             {'slug': slug},
             {'$set': {report_name: report}}
         )
 
-    def find(slug):
+    @classmethod
+    def find(cls, slug: str) -> typing.Dict:
         return db.db.agencies.find_one({'slug': slug}, {'_id': False})
 
-    def all():
+    @classmethod
+    def all(cls) -> typing.Iterable[typing.Dict]:
         return db.db.agencies.find({}, {'_id': False})
