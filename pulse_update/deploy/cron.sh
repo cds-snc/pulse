b3832a46cf41ec6ac28b948ecd73a5b2bc4c826a
David Buckley <buckley.w.david@gmail.com>
2018-05-15 19:24:27 -0400

    shuffle configuration to be at container init instead of on each cron job run

--- pulse_update/deploy/cron.sh
+++ pulse_update/deploy/cron.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 
-# Set the path to domain-scan.
-export DOMAIN_SCAN_PATH=/opt/scan/domain-scan/scan
-export DOMAIN_GATHER_PATH=/opt/scan/domain-scan/gather
-
-# Baseline where Pulse is checked out to
-export PULSE_HOME=/opt/scan/pulse
-
-# Make sure that AWS credentials have been configured
-mkdir -p ~/.aws
-cat > ~/.aws/credentials << EOF
-[lambda]
-aws_access_key_id=$PULSE_AWS_KEY_ID
-aws_secret_access_key=$PULSE_AWS_SECRET
-EOF 
-
-cat > ~/.aws/config << EOF
-[profile lambda]
-region=$PULSE_AWS_REGION
-output=json
-EOF
-
 aws lambda get-function --function-name task_sslyze > /dev/null
 SSLYZE=$?
 aws lambda get-function --function-name task_pshtt > /dev/null
@@ -38,5 +17,6 @@ if [[ $LAMBDA -eq 1 ]]
 then
     pulse run --scan here --lambda --lambda-profile lambda
 else
-    pusel run --scan here
+    pulse run --scan here
 fi
+rm -rf data/output
