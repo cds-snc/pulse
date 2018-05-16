e52c449a3d28c22bb294b17db5f667cbbddc701c
evadb <evademersbrett@gmail.com>
2018-05-16 17:33:47 -0400

    Start of mobile menu

--- pulse/pulse/static/js/main.js
+++ pulse/pulse/static/js/main.js
@@ -1,7 +1,7 @@
 $(function(){
 
   $('#menu-btn, .overlay, .sliding-panel-close').on('click touchstart',function (e) {
-    $('#menu-content, .overlay').toggleClass('is-visible');
+    $('#menu-content, .overlay').toggleClass('hidden');
 
     if($('#menu-content').hasClass('is-visible')) {
       $('#menu-content a').attr('tabindex', '0');
