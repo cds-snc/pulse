a83a23d17592e68517f5d9f5f1ce57bf9cee1936
evadb <evademersbrett@gmail.com>
2018-06-12 11:45:15 -0400

    JQuery functionality

--- track_digital/track/static/js/main.js
+++ track_digital/track/static/js/main.js
@@ -1,5 +1,7 @@
 $(function(){
 
+  var KEYCODE_ESC = 27;
+
   $('#menu-btn, .overlay, .sliding-panel-close').on('click touchstart',function (e) {
     $('#menu-content, .overlay').toggleClass('is-visible');
 
@@ -12,5 +14,23 @@ $(function(){
 
     e.preventDefault();
   });
+
+  $('#modal-btn').on('click touchstart', function (e) {
+    $('#modal').toggleClass('hidden flex');
+
+    e.preventDefault();
+  });
+
+  $('#close-btn').on('click touchstart', function (e) {
+    $('#modal').toggleClass('hidden flex');
+
+    e.preventDefault();
+  });
+
+  $(document).keyup(function(e) {
+    if(e.keyCode === KEYCODE_ESC) {
+      if($('#modal').hasClass('flex')) $('#modal').toggleClass('hidden flex');
+    };
+  })
 
 });
