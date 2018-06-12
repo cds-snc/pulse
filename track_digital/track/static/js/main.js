a9ffecccd41c337f6f698628d83da3213b44b2f9
evadb <evademersbrett@gmail.com>
2018-06-12 14:02:06 -0400

    focus management working

--- track_digital/track/static/js/main.js
+++ track_digital/track/static/js/main.js
@@ -2,6 +2,8 @@ $(function(){
 
   var KEYCODE_ESC = 27;
 
+  //Mobile menu
+
   $('#menu-btn, .overlay, .sliding-panel-close').on('click touchstart',function (e) {
     $('#menu-content, .overlay').toggleClass('is-visible');
 
@@ -15,22 +17,55 @@ $(function(){
     e.preventDefault();
   });
 
+  //Modal
+
   $('#modal-btn').on('click touchstart', function (e) {
-    $('#modal').toggleClass('hidden flex');
+    if ($('#modal').hasClass('hidden')) toggleModal('show');
 
     e.preventDefault();
   });
 
   $('#close-btn').on('click touchstart', function (e) {
-    $('#modal').toggleClass('hidden flex');
+    toggleModal('hide');
 
     e.preventDefault();
   });
 
   $(document).keyup(function(e) {
-    if(e.keyCode === KEYCODE_ESC) {
-      if($('#modal').hasClass('flex')) $('#modal').toggleClass('hidden flex');
-    };
+    if(e.keyCode === KEYCODE_ESC && $('#modal').hasClass('flex')) {
+      toggleModal('hide')
+    }
   })
+
+  $(document).focusin(function(e) {
+    var modal = document.getElementById("modal");
+
+    if($('#modal').hasClass('flex') && !$.contains(modal, e.target)) {
+      e.stopPropagation();
+      $('#modal-content').focus();
+    }
+
+  });
+
+  //function handler to toggle modal
+  var toggleModal = function(sh){ 
+    if(sh == "show") {
+      $('#modal').toggleClass('hidden flex');
+
+      //focus the header of the modal
+      $('#modal-content').focus();
+
+      //deactivate header/maincontent/footer
+      $('#header, #main-content, #goc-footer').attr('aria-hidden', 'true');
+      
+    }
+
+    else {
+      $('#modal').toggleClass('hidden flex');
+
+      //set focus back to modal btn
+    }
+
+  };
 
 });
