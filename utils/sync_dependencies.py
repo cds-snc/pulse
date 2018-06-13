304c474569fd31196e07b970d9236d4aff725acb
David Buckley <buckley.w.david@gmail.com>
2018-06-13 17:34:19 -0400

    don't use exec

--- utils/sync_dependencies.py
+++ utils/sync_dependencies.py
@@ -1,22 +1,30 @@
 #!/usr/bin/env python
 import argparse
+import importlib
+import contextlib
 import pathlib
 import typing
+import sys
+from unittest import mock
 
 import distutils
+import setuptools
+
+@contextlib.contextmanager
+def in_path(path: pathlib.Path):
+    sys.path.insert(0, str(path))
+    yield
+    sys.path.pop(0)
 
 
 def extract_setup_requires(setup_path: pathlib.Path) -> typing.Set[str]:
     with setup_path.open('r', encoding='utf-8-sig') as setup:
         content = setup.read()
 
-    try:
-        # Super evil `exec` usage. Long story short never run this function on anything ever
-        exec(content, globals(), locals()) # pylint: disable=exec-used
-    except SystemExit:
-        pass
-
-    return set(distutils.core._setup_distribution.install_requires) # pylint: disable
+    with in_path(setup_path.parent), mock.patch.object(setuptools, 'setup') as mock_setup:
+        importlib.import_module(setup_path.with_suffix('').name)
+        args, kwargs = mock_setup.call_args
+        return set(kwargs.get('install_requires', []))
 
 
 def extract_txt_requires(requirements_path: pathlib.Path) -> typing.Set[str]:
@@ -35,9 +43,13 @@ def are_syncd(*paths: pathlib.Path) -> bool:
     first = next(reqs, None)
     return all(first == other for other in reqs)
 
-if __name__ == '__main__':
+
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('path', type=str, nargs='+')
     args = parser.parse_args()
-    if not are_syncd(*[pathlib.Path(path) for path in args.path]):
-        exit(1)
+    return not are_syncd(*[pathlib.Path(path) for path in args.path])
+
+
+if __name__ == '__main__':
+    exit(main())
